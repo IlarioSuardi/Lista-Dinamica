3b0938024af6e9db04b69df584b260ac5580e5e0
Ilario <suardi.ilario.studente@itispaleocapa.it>
2026-03-06 12:42:19 +0100

    Add files via upload

---
+++ GestioneListaApplication.java
@@ -0,0 +1,19 @@
+package com.example.listadinamica;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class GestioneListaApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(GestioneListaApplication.class.getResource("lista-dinamica.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 440);
+        stage.setTitle("Lista Dinamica");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
---
+++ GestioneListaController.java
@@ -0,0 +1,104 @@
+package com.example.listadinamica;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class GestioneListaController {
+
+    @FXML
+    private TextField inserisciTesto;
+
+    @FXML
+    private TextField cercaTesto;
+
+    @FXML
+    private TextField nuovoTesto;
+
+    @FXML
+    private TextField messaggioStato;
+
+    @FXML
+    private TextArea output;
+
+    private Lista lista = new Lista();
+
+    // Aggiorna sempre l'output con la lista completa
+    private void aggiornaOutput() {
+        output.setText(lista.toString());
+    }
+
+    @FXML
+    protected void aggiungiClick() {
+        String valore = inserisciTesto.getText();
+
+        if (!valore.isEmpty()) {
+            lista.aggiungi(valore);
+            messaggioStato.setText("Elemento aggiunto!");
+            inserisciTesto.clear();
+            aggiornaOutput();
+        }
+    }
+
+    @FXML
+    protected void modificaClick() {
+        String vecchio = cercaTesto.getText();
+        String nuovo = nuovoTesto.getText();
+
+        if (lista.modifica(vecchio, nuovo)) {
+            messaggioStato.setText("Modifica effettuata!");
+        } else {
+            messaggioStato.setText("Elemento non trovato.");
+        }
+
+        cercaTesto.clear();
+        nuovoTesto.clear();
+        aggiornaOutput();
+    }
+
+    @FXML
+    protected void eliminaClick() {
+        String valore = cercaTesto.getText();
+
+        if (lista.elimina(valore)) {
+            messaggioStato.setText("Elemento eliminato!");
+        } else {
+            messaggioStato.setText("Elemento non trovato.");
+        }
+
+        cercaTesto.clear();
+        aggiornaOutput();
+    }
+
+    @FXML
+    protected void cercaClick() {
+        String valore = cercaTesto.getText();
+
+        lista.resetIteratore();
+        boolean trovato = false;
+        String elemento;
+        int posizione = 1;
+
+        while ((elemento = lista.visita()) != null) {
+            if (elemento.equals(valore)) {
+                trovato = true;
+                break;
+            }
+            posizione++;
+        }
+
+        if (trovato) {
+            messaggioStato.setText("Elemento trovato in posizione: " + posizione);
+        } else {
+            messaggioStato.setText("Elemento non trovato.");
+        }
+
+        cercaTesto.clear();
+        aggiornaOutput();
+    }
+
+    @FXML
+    protected void visualizzaClick() {
+        aggiornaOutput();
+    }
+}
