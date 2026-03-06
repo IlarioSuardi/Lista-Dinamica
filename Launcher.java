b83616ed7ec0ee661347959b84dd33f53fde4bf5
Ilario <suardi.ilario.studente@itispaleocapa.it>
2026-03-06 12:41:33 +0100

    Delete Launcher.java

--- Launcher.java
+++
@@ -1,9 +0,0 @@
-package com.example.listadinamica;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(GestioneListaApplication.class, args);
-    }
-}
